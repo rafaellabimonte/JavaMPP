f9d552d0dfb1f999c803b36ea99ed16dc23b5aa5
rafaellabimonte <rafaellabimonte09@gmail.com>
2026-02-09 11:43:35 -0300

    Add files via upload

---
+++ Exemplo1/src/exemplo1/Exemplo1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemplo1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Exemplo1 {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo !!!");
+        
+        int idade = 19; 
+        double altura = 1.55;
+        String nome = "Rafaella";
+        
+        System.out.println("Olá " + nome + ", seja bem vindo(a)!");
+        System.out.println("Sua altura é " + altura);
+        
+        double dias = idade * 365;
+        System.out.println("Sua idade é " + idade);
+        System.out.println("Você já viveu " + dias + " dias");
+    }
+    
+}
